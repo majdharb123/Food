5490dca7da7887f7963ea30396bd836a44cba628
Majd Harb <majd@gmail.com>
2026-01-04 21:42:44 +0200

    Switch to connection pool to fix fatal error

--- backend/db.js
+++ backend/db.js
@@ -1,15 +1,19 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "food"
-});
+const db = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: true
+    },
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
 
-db.connect(err => {
-  if (err) throw err;
-  console.log("MySQL Connected");
-});
+console.log("✅ Database Pool Created!");
 
 module.exports = db;
